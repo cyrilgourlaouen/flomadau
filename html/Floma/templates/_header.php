0c7139199e49f05e81126fa075e67b0f78d5965b
Cyril <cyril.gourlaouen@gmail.com>
2025-05-18 16:38:49 +0200

    chore: updated css balise

--- html/Floma/templates/_header.php
+++ html/Floma/templates/_header.php
@@ -1,41 +1,42 @@
 <header>
 	<div class="top_header">
-		<a href="#" id="BtnOpen" class="btnBurger"><img src="/assets/icons/menu-burger-white.svg" alt="menu burger"></a>
+		<a href="#" id="BtnOpen" class="btnBurger"><img src="/assets/icons/menu_burger_white.svg" alt="Icone de menu burger"></a>
 		<img class="logo" src="/assets/images/logo_entier_blanc.svg" alt="logo">
 		<div class="menu_nav">
 			<a href="#" class="PostOffer">
-				<h2>Publier une offre</h2>
+				<h3>Publier une offre</h3>
 			</a>
 			<a href="#" class="search"><img src="/assets/icons/search_white.svg" alt="loupe" class="ImgSearch"></a>
-			<a href="#" class="logIn"><img src="/assets/icons/logIn.svg" alt="logIn" class="ImgLogIn"></a>
+			<a href="#" class="logIn"><img src="/assets/icons/login_white.svg" alt="Icon de connexion" class="ImgLogIn"></a>
 		</div>
 	</div>
 
+	<!-- TODO: Ajouter les liens dur les pages -->
 	<div id="mySidenav" class="sidenav hidden">
 		<div class="menu">
-			<a id="BtnClose" href="#" class="close"><img src="/assets/icons/Flèche.svg" alt="flèche"></a>
-			<h1>Menu</h1>
+			<a id="BtnClose" href="#" class="close"><img src="/assets/icons/chevron_left_white.svg" alt="Chevron vers la gauche"></a>
+			<h2>Menu</h2>
 			<a href="#">
-				<img src="/assets/icons/discover.svg" alt="discover">
-				<h1>Découvrez nos offres</h1>
+				<img src="/assets/icons/discover_white.svg" alt="Icone boussole">
+				<h2>Découvrez nos offres</h2>
 			</a>
 			<a href="#">
-				<img src="/assets/icons/diamond.svg" alt="diamond">
-				<h1>Sélection du moment</h1>
+				<img src="/assets/icons/diamond_white.svg" alt="Icone de diamant">
+				<h2>Sélection du moment</h2>
 			</a>
 			<a href="#">
-				<img src="/assets/icons/recent_search.svg" alt="recent_search">
-				<h1>Consulté récement</h1>
+				<img src="/assets/icons/recent_search_white.svg" alt="Icone de recherche récente">
+				<h2>Consulté récement</h2>
 			</a>
 			<hr>
 			<a href="#">
-				<img src="/assets/icons/logIn.svg" alt="log in">
-				<h1>Connexion / Inscription</h1>
+				<img src="/assets/icons/login_white.svg" alt="Icone de connexion">
+				<h2>Connexion / Inscription</h2>
 			</a>
 		</div>
 		<nav>
-			<h2>Mentions légales</h2>
-			<h2>Politique de confidentialité</h2>
+			<h3>Mentions légales</h3>
+			<h3>Politique de confidentialité</h3>
 			<img src="/assets/images/logo_entier_blanc.svg" alt="logo">
 		</nav>
 	</div>
