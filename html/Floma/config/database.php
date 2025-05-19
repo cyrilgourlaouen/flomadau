07485953fc87588195d61a69e1143282600e2bdc
cgourlaouen <cyril22100@gmail.com>
2025-05-19 15:52:40 +0200

    chore: added schema

--- html/Floma/config/database.php
+++ html/Floma/config/database.php
@@ -2,10 +2,11 @@
 
 require_once __DIR__ . '/load_env.php';
 
-define("DB_INFOS", [
-    'host' => $_ENV['DB_HOST'],
-    'port' => $_ENV['DB_PORT'],
-    'dbname' => $_ENV['DB_NAME'],
-    'username' => $_ENV['DB_USERNAME'],
-    'password' => $_ENV['DB_PASSWORD']
+define('DB_INFOS', [
+    'host' => 'localhost',
+    'port' => 5432,
+    'dbname' => 'postgres',
+    'schema' => 'bdd_flomadau',
+    'username' => 'postgres',
+    'password' => 'dev'
 ]);
