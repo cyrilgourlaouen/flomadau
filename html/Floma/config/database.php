cabf92df9baa67166902d91e54eb889bbcddf8f5
cgourlaouen <cyril22100@gmail.com>
2025-05-19 15:55:54 +0200

    chore: added env vars

--- html/Floma/config/database.php
+++ html/Floma/config/database.php
@@ -3,10 +3,10 @@
 require_once __DIR__ . '/load_env.php';
 
 define('DB_INFOS', [
-    'host' => 'localhost',
-    'port' => 5432,
-    'dbname' => 'postgres',
-    'schema' => 'bdd_flomadau',
-    'username' => 'postgres',
-    'password' => 'dev'
+    'host' => $_ENV['DB_HOST'],
+    'port' => $_ENV['DB_PORT'],
+    'dbname' => $_ENV['DB_NAME'],
+    'schema' => $_ENV['DB_SCHEMA'],
+    'username' => $_ENV['DB_USERNAME'],
+    'password' => $_ENV['DB_PASSWORD']
 ]);
