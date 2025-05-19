b5aaf9dc0e0c2a21b88379792bc7ff056cb62b0a
Cyril <cyril.gourlaouen@gmail.com>
2025-05-19 15:58:54 +0200

    chore: added schema (#6)
    * chore: added schema
    
    * chore: added env vars

--- html/Floma/config/database.php
+++ html/Floma/config/database.php
@@ -2,10 +2,11 @@
 
 require_once __DIR__ . '/load_env.php';
 
-define("DB_INFOS", [
+define('DB_INFOS', [
     'host' => $_ENV['DB_HOST'],
     'port' => $_ENV['DB_PORT'],
     'dbname' => $_ENV['DB_NAME'],
+    'schema' => $_ENV['DB_SCHEMA'],
     'username' => $_ENV['DB_USERNAME'],
     'password' => $_ENV['DB_PASSWORD']
 ]);
