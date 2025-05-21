062cf58c9d8355d9dcbac30841ad084edb34ef1e
Cyril <cyril.gourlaouen@gmail.com>
2025-05-22 01:15:31 +0200

    feat: added all entities (#9)
    * feat: added all entities
    
    * chore: modified AbstractManager

--- html/Floma/src/Entity/Membre.php
+++ html/Floma/src/Entity/Membre.php
@@ -10,30 +10,12 @@ class Membre
     private string $pseudo;
     private int $id_compte;
 
-    public function getCode(): int
-    {
-        return $this->code;
-    }
-    public function setCode(int $code): void
-    {
-        $this->code = $code;
-    }
+    public function getCode(): int { return $this->code; }
+    public function setCode(int $code): void { $this->code = $code; }
 
-    public function getPseudo(): string
-    {
-        return $this->pseudo;
-    }
-    public function setPseudo(string $pseudo): void
-    {
-        $this->pseudo = $pseudo;
-    }
+    public function getPseudo(): string { return $this->pseudo; }
+    public function setPseudo(string $pseudo): void { $this->pseudo = $pseudo; }
 
-    public function getIdCompte(): int
-    {
-        return $this->id_compte;
-    }
-    public function setIdCompte(int $id_compte): void
-    {
-        $this->id_compte = $id_compte;
-    }
+    public function getIdCompte(): int { return $this->id_compte; }
+    public function setIdCompte(int $id_compte): void { $this->id_compte = $id_compte; }
 }
