db38d173c236ea361505ae35ed76035e44a02ff1
lgf <jonas.brisson@etudiant.univ-rennes1.fr>
2025-05-22 23:01:33 +0200

    Création de la barre de recherche plus des btn à coté

--- html/Floma/templates/backoffice/home.php
+++ html/Floma/templates/backoffice/home.php
@@ -1,4 +1,6 @@
 <?php
+    include 'button.php';
+
     $nom_entreprise ="";
     $nb_offres = 0;
     $nb_avis = 0;
@@ -30,4 +32,25 @@
     $head_svg = "/assets/icons/facture_white.svg";
     include 'little_title.php';
 ?>
+</div>
+<div id="title_searchbar_btn">
+    <h2 id="all_offer">Toutes mes offres</h2>
+    <div id="searchbar_all_btn">
+        <div id="searchbar_white_btn">
+            <form action="" method="">
+                <input type="search" id="input_recherche" name="recherche" placeholder="Rechercher" disabled>
+            </form>
+            <?php 
+                echo button('Filtrer', 'btn_filtrer');    
+            ?>
+            <?php 
+                echo button('Trier', 'btn_trier');    
+            ?>
+        </div>
+        <div id="blue_btn">
+            <?php 
+                echo button('Ajouter', 'btn_ajouter');    
+            ?>
+        </div>
+    </div>
 </div>
