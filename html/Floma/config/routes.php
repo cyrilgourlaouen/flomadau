b30efe85a2566daa6e98f04404fbd3f8154be190
Alexis Gandemer <alexis.gandemer@etudiant.univ-rennes1.fr>
2025-05-22 17:42:44 +0200

    Feat:Implémentation de la page de connexion pour les pros

--- html/Floma/config/routes.php
+++ html/Floma/config/routes.php
@@ -12,4 +12,9 @@ const ROUTES = [
         'method' => 'home',
         'layout' => Layout::BACK,
     ],
+    '/pro/connexion' => [
+        'controller' => App\Controller\MainProController::class,
+        'method' => 'home',
+        'layout' => Layout::BACK,
+    ],
 ];
