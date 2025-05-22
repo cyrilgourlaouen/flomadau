f5d1a9b035fe09a45688ef104dbe329a8675a67b
Cyril <cyril.gourlaouen@gmail.com>
2025-05-22 17:46:27 +0200

    chore: modifed  home.php

--- html/Floma/templates/front/main/home.php
+++ html/Floma/templates/front/main/home.php
@@ -2,14 +2,12 @@
 use App\Service\MetricStarsCalculator;
 use App\Enum\OfferCategoryEnum;
 use App\Enum\OptionVisibiliteEnum;
-use App\Service\RestaurantPriceRangeCalculator;
 
 $starCalculator = new MetricStarsCalculator();
-$euroCalculator = new RestaurantPriceRangeCalculator();
 ?>
 
 <!-- Section Offre -->
-<section>
+<section class="offer-section">
     <h2>Découvrez nos offres</h2>
 
     <div class="offer-list">
@@ -17,54 +15,53 @@ $euroCalculator = new RestaurantPriceRangeCalculator();
             <article class="offer-card">
                 <!-- Image -->
                 <div class="offer-card-img">
-    
                     <?php
                     $optionsData = $offer['optionVisibiliteData'] ?? null;
-    
+
                     $showPink = false;
                     $showGreen = false;
-                
+
                     if ($optionsData) {
                         if (array_key_exists('nom_option', $optionsData)) {
                             $optionsData = [$optionsData];
                         }
-    
+
                         foreach ($optionsData as $option) {
                             $label = $option['nom_option'] ?? null;
-    
+
                             if ($label === OptionVisibiliteEnum::ALaUne->value) {
                                 $showPink = true;
                                 $showGreen = true;
                                 break;
                             }
-    
+
                             if ($label === OptionVisibiliteEnum::EnRelief->value) {
                                 $showGreen = true;
                             }
                         }
                     }
                     ?>
-    
+
                     <?php if ($showPink || $showGreen) { ?>
                         <div class="offer-card-img-badges">
-    
+
                             <?php if ($showPink) { ?>
                                 <div class="offer-card-img-badge-pink">
                                     <img src="/assets/icons/diamond_white.svg" alt="Icône diamant">
                                     <p class="very-small-text">Mis en avant</p>
                                 </div>
                             <?php } ?>
-    
+
                             <?php if ($showGreen) { ?>
                                 <div class="offer-card-img-badge-green">
                                     <img src="/assets/icons/editor_choice_white.svg" alt="Icône choix éditeurs">
                                     <p class="very-small-text">Conseillé par nos équipes</p>
                                 </div>
                             <?php } ?>
-    
+
                         </div>
                     <?php } ?>
-    
+
                     <!-- Icone de catégorie -->
                     <?php
                     $icon = OfferCategoryEnum::tryFrom($offer['categorie'])?->getIcon();
@@ -94,54 +91,61 @@ $euroCalculator = new RestaurantPriceRangeCalculator();
                     }
                     ?>
                     <?php if ($imageUrl) { ?>
-                        <img src="/uploads/offers/<?= htmlspecialchars($imageUrl) ?>" alt="Image de l'offre" class="offer-card-img-main">
-                    <?php }?>
+                        <img src="/uploads/offers/<?= htmlspecialchars($imageUrl) ?>" alt="Image de l'offre"
+                            class="offer-card-img-main">
+                    <?php } ?>
 
                 </div>
-    
-                <!-- Description -->
-                <h3><?= htmlspecialchars($offer['titre']) ?></h3>
-                <p><?= htmlspecialchars($offer['resume']) ?></p>
-    
-                <div class="offer-card-price-note">
-                    <!-- Note -->
-                    <div class="offer-card-note">
-                        <div class="offer-card-note-stars">
-                            <?= $starCalculator->calculStars($offer['note_moyenne']); ?>
-                        </div>
-                        <p>(<?= htmlspecialchars($offer['nombre_avis']) ?>)</p>
+
+                <div class="offer-card-info-layout">
+                    <!-- Description -->
+                    <div class="offer-card-gap">
+                        <h3><?= htmlspecialchars($offer['titre']) ?></h3>
+                        <p><?= htmlspecialchars($offer['resume']) ?></p>
                     </div>
-    
-                    <!-- Prix -->
-                    <?php if ($offer['categorie'] != OfferCategoryEnum::Restauration->value) {
-                        if (isset($offer['categoryData'])) { ?>
-                            <div class="offer-card-price">
-                                <p><?= $offer['categoryData']['prix_minimal'] == 0 ? "Gratuit" : $offer['categoryData']['prix_minimal'] ?></p>
-                                <img src="/assets/icons/euro_symbol_primary.svg" alt="Icone d'euro">
+
+                    <div class="offer-card-gap">
+                        <div class="offer-card-price-note">
+                            <!-- Note -->
+                            <div class="offer-card-note">
+                                <div class="offer-card-note-stars">
+                                    <?= $starCalculator->calculStars($offer['note_moyenne']); ?>
+                                </div>
+                                <p>(<?= htmlspecialchars($offer['nombre_avis']) ?>)</p>
                             </div>
-                        <?php } else { ?>
-                            <div class="offer-card-price">
-                                <p>Min. 0</p>
-                                <img src="/assets/icons/euro_symbol_primary.svg" alt="Icone d'euro">
-                            </div>
-                        <?php } ?>
-                    <?php } else { ?>
-                        <div class="offer-card-price-euros">
-                            <?= $euroCalculator->calculEuros($offer['categoryData']['gamme_de_prix']) ?>
+
+                            <!-- Prix -->
+                            <?php if ($offer['categorie'] != OfferCategoryEnum::Restauration->value) {
+                                if (isset($offer['categoryData'])) { ?>
+                                    <div class="offer-card-price">
+                                        <p><?= $offer['categoryData']['prix_minimal'] == 0 ? "Gratuit" : $offer['categoryData']['prix_minimal'] ?>
+                                        </p>
+                                        <img src="/assets/icons/euro_symbol_primary.svg" alt="Icone d'euro">
+                                    </div>
+                                <?php } else { ?>
+                                    <div class="offer-card-price">
+                                        <p>Min. 0</p>
+                                        <img src="/assets/icons/euro_symbol_primary.svg" alt="Icone d'euro">
+                                    </div>
+                                <?php } ?>
+                            <?php } else { ?>
+                                <div class="offer-card-price-euros">
+                                    <?= str_repeat("<img src='/assets/icons/euro_symbol_primary.svg' alt='Icone d'euro'>", $offer["categoryData"]["gamme_de_prix"]) ?>
+                                </div>
+                            <?php } ?>
                         </div>
-                    <?php } ?>
-                </div>
-    
-                <div class="offer-card-city-author">
-                    <!-- Ville -->
-                    <div class="offer-card-city">
-                        <img src="/assets/icons/location_on_primary.svg" alt="">
-                        <p><?= htmlspecialchars($offer['ville']) ?></p>
+
+                        <div class="offer-card-city-author">
+                            <!-- Ville -->
+                            <div class="offer-card-city">
+                                <img src="/assets/icons/location_on_primary.svg" alt="">
+                                <p><?= htmlspecialchars($offer['ville']) ?></p>
+                            </div>
+
+                            <!-- Auteur -->
+                            <p class="italic"><?= $offer['professionnelData']['raison_sociale'] ?></p>
+                        </div>
                     </div>
-    
-                    <!-- Auteur -->
-                    <p class="italic"><?= $offer['professionnelData']['raison_sociale'] ?></p>
-    
                 </div>
             </article>
         <?php } ?>
