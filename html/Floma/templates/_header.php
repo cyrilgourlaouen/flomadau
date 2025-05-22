a40ab0ff8a5ab81a62ae6d0c854c32fb248362b7
djulou <dorian.julou@gmail.com>
2025-05-23 00:11:51 +0200

    Fix: CSS

--- html/Floma/templates/_header.php
+++ html/Floma/templates/_header.php
@@ -1,26 +1,29 @@
 <header>
 	<div class="top_header">
-		<a href="#" id="BtnOpen" class="btnBurger"><img src="/assets/icons/menu_burger_white.svg" alt="Icone de menu burger"></a>
+		<a href="#" id="BtnOpen" class="btnBurger"><img src="/assets/icons/menu_burger_white.svg"
+				alt="Icone de menu burger"></a>
 		<a href="/"><img class="logo" src="/assets/images/logo_entier_blanc.svg" alt="logo"></a>
 		<div class="menu_nav">
 			<a href="#" class="postOffer">
 				<h3 class="txtPostOffer">Publier une offre</h3>
 			</a>
 			<a href="#" class="search"><img src="/assets/icons/search_white.svg" alt="loupe" class="ImgSearch"></a>
-			<a href="#" class="logIn"><img src="/assets/icons/login_white.svg" alt="Icon de connexion" class="ImgLogIn"></a>
+			<a href="#" class="logIn"><img src="/assets/icons/login_white.svg" alt="Icon de connexion"
+					class="ImgLogIn"></a>
 		</div>
 	</div>
 
 	<!-- TODO: Ajouter les liens dur les pages -->
 	<div id="mySidenav" class="sidenav hidden">
 		<div class="menu">
-			<a id="BtnClose" href="#" class="close"><img src="/assets/icons/chevron_left_white.svg" alt="Chevron vers la gauche"></a>
+			<a id="BtnClose" href="#" class="close"><img src="/assets/icons/chevron_left_white.svg"
+					alt="Chevron vers la gauche"></a>
 			<h2>Menu</h2>
 			<a href="#" class="action">
 				<img src="/assets/icons/discover_white.svg" alt="Icone boussole">
 				<h2>Découvrez nos offres</h2>
 			</a>
-			<a href="#" class="action"> 
+			<a href="#" class="action">
 				<img src="/assets/icons/diamond_white.svg" alt="Icone de diamant">
 				<h2>Sélection du moment</h2>
 			</a>
@@ -28,7 +31,7 @@
 				<img src="/assets/icons/recent_search_white.svg" alt="Icone de recherche récente">
 				<h2>Consulté récement</h2>
 			</a>
-			<hr>	
+			<hr>
 			<a href="#" class="action">
 				<img src="/assets/icons/login_white.svg" alt="Icone de connexion">
 				<h2>Connexion / Inscription</h2>
@@ -45,5 +48,18 @@
 		</nav>
 	</div>
 	<script src="./js/_js-header.js"></script>
-</header>
 
+	<div id="loginModal" class="modal hidden">
+		<div class="modal-content">
+			<span class="close">&times;</span>
+			<h2>Connexion</h2>
+			<form>
+				<label for="email">Email :</label>
+				<input type="email" id="email" name="email" required>
+				<label for="password">Mot de passe :</label>
+				<input type="password" id="password" name="password" required>
+				<button type="submit">Se connecter</button>
+			</form>
+		</div>
+	</div>
+</header>
