ec5e8d1184ebaa85c2bb1661ad68347a498f2c0e
lgf-99 <jonas.brisson@etudiant.univ-rennes1.fr>
2025-05-22 08:51:41 +0200

    Ajout du test bdd dans le home.php

--- html/Floma/templates/main/home.php
+++ html/Floma/templates/main/home.php
@@ -1,2 +1,33 @@
 <h1>Floma, un micro framework PHP</h1>
-<p>Test</p>
+<p>Test</p><?php
+    // Informations de connexion à la base de données
+    $host = DB_HOST;
+    $dbName = DB_NAME;
+    $username = DB_USERNAME;
+    $password = DB_PASSWORD;
+    $port = DB_PORT;
+    $schema = DB_SCHEMA;
+    
+    function connect(): PDO
+    {
+        $dsn = sprintf(
+            "pgsql:host=%s;port=%s;dbname=%s;options='--search_path=%s'",
+            $host,
+            $port,
+            $dbName,
+            $schema
+        );
+
+        $db = new PDO($dsn, $username, $password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $db;
+    }
+
+    $db = connect();
+    $stmt = $db->prepare("SELECT * FROM Compte");
+    $stmt->execute();
+
+    $res = $stmt->fetchAll();
+    print_r($res);
+?>
