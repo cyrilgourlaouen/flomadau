26c66527b8683fc12bcfb583a46f955eaaa6d319
Nathan <bzbzz@2a01cb011026dacff5085b3c404e0e3d.ipv6.abo.wanadoo.fr>
2025-05-22 19:38:09 +0200

    feat: account creation on form submition

--- html/Floma/src/Controller/SignupProController.php
+++ html/Floma/src/Controller/SignupProController.php
@@ -2,22 +2,55 @@
 
 namespace App\Controller;
 
+use App\Entity\Compte;
 use Floma\Controller\AbstractController;
-use Floma\View\Layout;
+use App\Manager\CompteProManager;
 
 class SignupProController extends AbstractController
 {
-    public function show()
+    public function page()
     {
-        $this->setLayout(Layout::BACK);
-        
         return $this->renderView(
             'backoffice/signup_page.php',
             [
                 'seo' => [
                     'title' => 'Accueil professionnel',
-                    'descriptions'=> 'Page d\'accueil d\'un professionnel PACT, parcourez vos offres, lisez vos avis.'
+                    'descriptions' => 'Page d\'accueil d\'un professionnel PACT, parcourez vos offres, lisez vos avis.'
                 ]
-            ]);
+            ]
+        );
+    }
+
+    public function submit()
+    {
+        if (!empty($_POST)) {
+            $comptePro = new Compte();
+            $compteProManager = new CompteProManager();
+
+            $comptePro->setNom($_POST['nom'] ?? null);
+            $comptePro->setPrenom($_POST['prenom'] ?? null);
+            $comptePro->setEmail($_POST['mail'] ?? null);
+            $comptePro->setTelephone($_POST['num'] ?? null);
+            $comptePro->setMotDePasse($_POST['mdp'] ?? null);
+            $comptePro->setVille($_POST['ville'] ?? null);
+            $comptePro->setCodePostal($_POST['code_postal'] ?? null);
+            $comptePro->setNomRue($_POST['rue'] ?? null);
+            $comptePro->setNumeroRue($_POST['numero'] ?? null);
+            $comptePro->setComplementAdresse($_POST['complement'] ?? null);
+
+            $compteProManager->add($comptePro);
+
+            return $this->redirectToRoute('/pro');
+        }
+
+        return $this->renderView(
+            'backoffice/signup_form.php',
+            [
+                'seo' => [
+                    'title' => 'Inscription professionnel',
+                    'descriptions' => 'Inscrivez-vous en tant que professionnel sur Floma.'
+                ]
+            ]
+        );
     }
 }
