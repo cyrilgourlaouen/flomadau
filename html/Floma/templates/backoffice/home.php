a5278f202320b95bbf4ca22513b062996c41da0b
lgf <jonas.brisson@etudiant.univ-rennes1.fr>
2025-05-22 19:16:00 +0200

    Ajout header + nav page acceuil pro

--- html/Floma/templates/backoffice/home.php
+++ html/Floma/templates/backoffice/home.php
@@ -1,1 +1,33 @@
-<p>Page d'accueil d'un professionnel</p>
+<?php
+    $nom_entreprise ="";
+    $nb_offres = 0;
+    $nb_avis = 0;
+    $mois = "";
+
+    //En-tête
+    $head_title = "ACCEUIL";
+    $head_subtitle = "LEGO$nom_entreprise";
+    $head_svg = "/assets/icons/account_white.svg";
+    include 'head_title.php';
+?>
+<div id="recap_menu">
+<?php
+    //Sous-en-tête pour offre
+    $head_title = "OFFRES";
+    $head_subtitle = $nb_offres;
+    $head_svg = "/assets/icons/offer_white.svg";
+    include 'little_title.php';
+
+    //Sous-en-tête pour avis
+    $head_title = "AVIS";
+    $head_subtitle = $nb_avis;
+    $head_svg = "/assets/icons/avis_white.svg";
+    include 'little_title.php';
+
+    //Sous-en-tête pour facture
+    $head_title = "FACTURE : MAI$mois";
+    $head_subtitle = "0€";
+    $head_svg = "/assets/icons/facture_white.svg";
+    include 'little_title.php';
+?>
+</div>
--- html/Floma/templates/backoffice/little_title.php
+++ html/Floma/templates/backoffice/little_title.php
@@ -3,16 +3,16 @@
     $head_title = $head_title ?? 'Default Title';
     $head_subtitle = $head_subtitle ?? 'Default Subtitle';
     $head_svg = $head_svg ?? '/assets/icons/account_white.svg';
-    ?>
+?>
     <link rel="stylesheet" type="text/css" href="back.css">
-    <div class="headContainer">
-        <div class="headContent">
-            <div class="boxIcon">
-                <img src="<?php echo htmlspecialchars($head_svg); ?>" alt="Account Icon" class="icon">
-            </div>
+    <div id="headContainer_little">
+        <div id="headContent_little">
             <div class="flexColumn">
                 <h1><?php echo htmlspecialchars($head_title); ?></h1>
-                <h2><?php echo htmlspecialchars($head_subtitle); ?></h2>
+                <h2 id="little_subtittle"><?php echo htmlspecialchars($head_subtitle); ?></h2>
+            </div>
+            <div class="boxIcon">
+                <img src="<?php echo htmlspecialchars($head_svg); ?>" alt="Account Icon" class="icon">
             </div>
         </div>
     </div>
