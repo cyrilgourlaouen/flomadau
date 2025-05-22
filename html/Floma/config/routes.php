37875056b62dea93e7ff80776bab91fd40678b10
OuestToast <99141779+Victorlesarb@users.noreply.github.com>
2025-05-23 00:00:21 +0200

    Feat/2.19.1/consulter detail offre/master (#14)
    * ajout code
    
    * Rebase sur le main
    
    * ajout fonctionnalité d'offres détaillés
    
    * fonctionnalité apres rebase sur le main
    
    * Rebase sur le main
    
    * ajout fonctionnalité d'offres détaillés
    
    * ajout image bbd + refacto css et html
    
    * ajout transition a 65rem
    
    * Ajout enum refactor style.css suppresion virgule en trop
    
    * ajustement image tablette et augmentation taille chevron
    
    * ajout gap dans comment nous rejoindre

--- html/Floma/config/routes.php
+++ html/Floma/config/routes.php
@@ -10,6 +10,14 @@ const ROUTES = [
     '/pro/connexion' => [
         'controller' => App\Controller\ConnexionProController::class,
         'method' => 'connexionPro',
+    ],
+    '/offer/{id}' => [
+        'controller' => App\Controller\OfferController::class,
+        'method' => 'show',
+    ],
+    '/pro' => [
+        'controller' => App\Controller\MainProController::class,
+        'method' => 'home',
         'layout' => Layout::BACK,
     ],
 ];
