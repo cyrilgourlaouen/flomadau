31fac94df53e15aab782a0696f3f7b42deb9a97c
Nathan <bzbzz@2a01cb011026dacff5085b3c404e0e3d.ipv6.abo.wanadoo.fr>
2025-05-22 23:11:46 +0200

    fix: not working links on menu are hidden

--- html/Floma/templates/_header_back.php
+++ html/Floma/templates/_header_back.php
@@ -4,21 +4,21 @@
         <div class="menuTextContainer">
             <h2>MENU</h2>
             <ul class="sidebar-menu">
-                <li class="menu-home"><a href="#">
+                <li class="menu-home"><a href="index.php?path=/pro">
                         <h2>Accueil</h2>
                     </a></li>
-                <li class="menu-info"><a href="#">
+                <li class="menu-info"><a href="index.php?path=/pro/signup">
                         <h2>Informations</h2>
                     </a></li>
-                <li class="menu-offers"><a href="#">
-                        <h2>Offres</h2>
-                    </a></li>
-                <li class="menu-avis"><a href="#">
-                        <h2>Avis</h2>
-                    </a></li>
-                <li class="menu-factures"><a href="#">
-                        <h2>Factures</h2>
-                    </a></li>
+                <!-- <li class="menu-offers"><a href="#"> -->
+                <!--         <h2>Offres</h2> -->
+                <!--     </a></li> -->
+                <!-- <li class="menu-avis"><a href="#"> -->
+                <!--         <h2>Avis</h2> -->
+                <!--     </a></li> -->
+                <!-- <li class="menu-factures"><a href="#"> -->
+                <!--         <h2>Factures</h2> -->
+                <!--     </a></li> -->
             </ul>
         </div>
     </div>
