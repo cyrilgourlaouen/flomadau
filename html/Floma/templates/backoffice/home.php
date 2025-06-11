12bc15ab4275f811ac90cc22bc78e738e64cdda3
lgf <jonas.brisson@etudiant.univ-rennes1.fr>
2025-06-11 18:32:46 +0200

    header et nav ok pour l'accueil back office (cassés jsp pk)

--- html/Floma/templates/backoffice/home.php
+++ html/Floma/templates/backoffice/home.php
@@ -9,7 +9,12 @@
 
         $nb_offres = 0;
         $nb_avis = 0;
-        $mois = "MAI";
+
+        /*$now = new DateTime();
+        $formatter = new IntlDateFormatter( "fr_FR" , IntlDateFormatter::NONE, IntlDateFormatter::NONE, null, null, 'MMMM');
+        $mois = $formatter->format($now);
+        echo($mois);*/
+        $mois = 'JUIN';
 
         //En-tête
         $head_title = "ACCEUIL";
@@ -19,25 +24,18 @@
     ?>
 </div>
 <div id="body_acceuil">
-    <div class="msg_offer">
-        <p class="text_offer">Vous souhaitez bénéficier des options professionnelles ?</p>
-        <div id="btn_co_inscription">
-                <button id="btn_connexion"><a href="?path=/pro/connexion">Connexion</a></button>
-                <button id="btn_inscription"><a href="?path=/pro/signup">Inscription</a></button>
-        </div>
-    </div>
-    <!--<?php
-        if(!isset($_SESSION['id'])){
+    <?php
+        if(/*!*/isset($_SESSION['id'])){
             ?>
                 <div class="msg_offer">
                     <p class="text_offer">Vous souhaitez bénéficier des options professionnelles ?</p>
                     <div id="btn_co_inscription">
-                            <button id="btn_connexion"><a href="?path=/pro/connexion">Connexion</a></button>
-                            <button id="btn_inscription"><a href="?path=/pro/inscription">Inscription</a><button>;
+                        <button id="btn_connexion"><a href="?path=/pro/connexion">Connexion</a></button>
+                        <button id="btn_inscription"><a href="?path=/pro/inscription">Inscription</a></button>
                     </div>
                 </div>
             <?php
-        }else if(!empty($data["offers"])){
+        }else if(empty($data["offers"])){
             ?>
                 <div>
                     <div class="msg_offer">
@@ -46,118 +44,120 @@
                 </div>
             <?php
         }else{
-    ?>-->
-        </div>
+            include 'nav_searchbar.php';
+    ?>
         <div class="list_offer">
-            <?php foreach ($data["offers"] as $offer) { ?>
-                <article class="offer-card">
-                    <!-- Image -->
-                    <div class="offer-card-img">
+    <?php
+        foreach ($data["offers"] as $offer) { ?>
+        <article class="offer-card">
+            <!-- Image -->
+            <div class="offer-card-img">
 
-                        <!-- Image de fond de l'offre -->
-                        <?php
-                            $imageUrl = null;
+                <!-- Image de fond de l'offre -->
+                <?php
+                    $imageUrl = null;
 
-                            if (!empty($offer['imageData']) && is_array($offer['imageData'])) {
-                                // Cherche l'image principale en priorité
-                                foreach ($offer['imageData'] as $img) {
-                                    if ($img['principale']) {
-                                        $imageUrl = $img['url_img'] ?? null;
-                                        break;
-                                    }
+                    if (!empty($offer['imageData']) && is_array($offer['imageData'])) {
+                        // Cherche l'image principale en priorité
+                        foreach ($offer['imageData'] as $img) {
+                            if ($img['principale']) {
+                                $imageUrl = $img['url_img'] ?? null;
+                                break;
+                            }
+                        }
+                    }
+                ?>
+                <?php if ($imageUrl) { ?>
+                    <img src="/uploads/offers/<?= htmlspecialchars($imageUrl) ?>" alt="Image de l'offre" class="offer-card-img">
+                <?php } ?>
+            </div>
+            <div class="offer-card-info-layout">
+                <!-- Description -->
+                <div class="offer-card-gap">
+                    <h3><?= htmlspecialchars($offer['titre']) ?></h3>
+                    <p><?= htmlspecialchars($offer['resume']) ?></p>
+                </div>
+
+                <div class="offer-card-category-prix-lieu">
+                    <!-- Catégorie -->
+                    <p>Catégorie : <?php echo $offer['categorie'];?></p>
+
+                    <!-- Prix -->
+                    <?php if ($offer['categorie'] != OfferCategoryEnum::Restauration->value) {
+                        if (isset($offer['categoryData'])) { ?>
+                            <div class="offer-card-price">
+                                <p><?= $offer['categoryData']['prix_minimal'] == 0 ? "Gratuit" : $offer['categoryData']['prix_minimal'] ?>
+                                </p>
+                                <img src="/assets/icons/euro_symbol_primary.svg" alt="Icone d'euro">
+                            </div>
+                        <?php } else { ?>
+                            <div class="offer-card-price">
+                                <p>Min. 0</p>
+                                <img src="/assets/icons/euro_symbol_primary.svg" alt="Icone d'euro">
+                            </div>
+                        <?php } ?>
+                    <?php } else { ?>
+                        <div class="offer-card-price-euros">
+                            <?= str_repeat("<img src='/assets/icons/euro_symbol_primary.svg' alt='Icone d'euro'>", $offer["categoryData"]["gamme_de_prix"]) ?>
+                        </div>
+                    <?php } ?>
+
+                    <!-- Ville -->
+                    <div class="offer-card-city">
+                        <img src="/assets/icons/location_primary.svg" alt="Icone de localisation">
+                        <p><?= htmlspecialchars($offer['ville']) ?></p>
+                    </div>
+                </div>
+
+                <div class="offer-card-price-note">
+                    <!-- Note -->
+                    <div class="offer-card-note">
+                        <div class="offer-card-note-stars">
+                            <?= $starCalculator->calculStars($offer['note_moyenne']); ?>
+                        </div>
+                        <p>(<?= htmlspecialchars($offer['nombre_avis']) ?>)</p>
+                    </div>
+                    <p>Aucun avis non consulté</p>
+                    <p>Aucun avis non répondu</p>
+                </div>
+            </div>
+            <div id="offer-card-right-layout">
+                <div id="offer-online-visibility">
+                    <!--Options de vibilité choisies-->
+                    <?php
+                        $optionsData = $offer['optionVisibiliteData'] ?? null;
+
+                        $une = false;
+                        $relief = false;
+
+                        if ($optionsData) {
+                            if (array_key_exists('nom_option', $optionsData)) {
+                                $optionsData = [$optionsData];
+                            }
+
+                            foreach ($optionsData as $option) {
+                                $label = $option['nom_option'] ?? null;
+
+                                if ($label === OptionVisibiliteEnum::ALaUne->value) {
+                                    ?>
+                                        <p>A la une</p>
+                                    <?php
+                                }
+
+                                if ($label === OptionVisibiliteEnum::EnRelief->value) {
+                                    ?>
+                                        <p>A la une</p>
+                                    <?php
                                 }
                             }
-                        ?>
-                        <?php if ($imageUrl) { ?>
-                            <img src="/uploads/offers/<?= htmlspecialchars($imageUrl) ?>" alt="Image de l'offre" class="offer-card-img">
-                        <?php } ?>
-                    </div>
-                    <div class="offer-card-info-layout">
-                        <!-- Description -->
-                        <div class="offer-card-gap">
-                            <h3><?= htmlspecialchars($offer['titre']) ?></h3>
-                            <p><?= htmlspecialchars($offer['resume']) ?></p>
-                        </div>
-
-                        <div class="offer-card-category-prix-lieu">
-                            <!-- Catégorie -->
-                            <p>Catégorie : <?php echo $offer['categorie'];?></p>
-
-                            <!-- Prix -->
-                            <?php if ($offer['categorie'] != OfferCategoryEnum::Restauration->value) {
-                                if (isset($offer['categoryData'])) { ?>
-                                    <div class="offer-card-price">
-                                        <p><?= $offer['categoryData']['prix_minimal'] == 0 ? "Gratuit" : $offer['categoryData']['prix_minimal'] ?>
-                                        </p>
-                                        <img src="/assets/icons/euro_symbol_primary.svg" alt="Icone d'euro">
-                                    </div>
-                                <?php } else { ?>
-                                    <div class="offer-card-price">
-                                        <p>Min. 0</p>
-                                        <img src="/assets/icons/euro_symbol_primary.svg" alt="Icone d'euro">
-                                    </div>
-                                <?php } ?>
-                            <?php } else { ?>
-                                <div class="offer-card-price-euros">
-                                    <?= str_repeat("<img src='/assets/icons/euro_symbol_primary.svg' alt='Icone d'euro'>", $offer["categoryData"]["gamme_de_prix"]) ?>
-                                </div>
-                            <?php } ?>
-
-                            <!-- Ville -->
-                            <div class="offer-card-city">
-                                <img src="/assets/icons/location_primary.svg" alt="Icone de localisation">
-                                <p><?= htmlspecialchars($offer['ville']) ?></p>
-                            </div>
-                        </div>
-
-                        <div class="offer-card-price-note">
-                            <!-- Note -->
-                            <div class="offer-card-note">
-                                <div class="offer-card-note-stars">
-                                    <?= $starCalculator->calculStars($offer['note_moyenne']); ?>
-                                </div>
-                                <p>(<?= htmlspecialchars($offer['nombre_avis']) ?>)</p>
-                            </div>
-                            <p>Aucun avis non consulté</p>
-                            <p>Aucun avis non répondu</p>
-                        </div>
-                    </div>
-                    <div id="offer-card-right-layout">
-                        <div id="offer-online-visibility">
-                            <!--Options de vibilité choisies-->
-                            <?php
-                                $optionsData = $offer['optionVisibiliteData'] ?? null;
-
-                                $une = false;
-                                $relief = false;
-
-                                if ($optionsData) {
-                                    if (array_key_exists('nom_option', $optionsData)) {
-                                        $optionsData = [$optionsData];
-                                    }
-
-                                    foreach ($optionsData as $option) {
-                                        $label = $option['nom_option'] ?? null;
-
-                                        if ($label === OptionVisibiliteEnum::ALaUne->value) {
-                                            ?>
-                                                <p>A la une</p>
-                                            <?php
-                                        }
-
-                                        if ($label === OptionVisibiliteEnum::EnRelief->value) {
-                                            ?>
-                                                <p>A la une</p>
-                                            <?php
-                                        }
-                                    }
-                                }
-                            ?>
-                        </div>
-                    </div>
-                </article>
-            <?php 
-                }
+                        }
+                    ?>
+                </div>
+            </div>
+        </article>
+    <?php 
             }
-            ?>
-        </div>
+        }
+    ?>
+    </div>
+</div>
