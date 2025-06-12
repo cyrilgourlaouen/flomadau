4770446eb857ace3b515f886746b05d8760f3513
Victor <victorlesarb35@gmail.com>
2025-06-12 17:48:59 +0200

    changement denomination sociale to email et ajout code prop dans la session

--- html/Floma/src/Service/MetricProAccount.php
+++ html/Floma/src/Service/MetricProAccount.php
@@ -7,9 +7,23 @@ class MetricProAccount
     public function isProExist(mixed $proAccount, $raisonSociale, $password)
     {
         foreach($proAccount as $account) {
+            $accountIds = [];
             if ($account["roleData"]) {
-                if ($account["roleData"][0]["raison_sociale"] == $raisonSociale && password_verify($password, $account["mot_de_passe"])){
+                array_push($accountIds, $account["id"]);
+                if ($account["email"] === $raisonSociale && password_verify($password, $account["mot_de_passe"]) && $account["roleData"][0]["id_compte"] === $account["id"]){
                     return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public function getProId(mixed $proAccount, $raisonSociale, $password): string
+    {
+        foreach($proAccount as $account) {
+            if ($account["roleData"]) {
+                if ($account["email"] == $raisonSociale && password_verify($password, $account["mot_de_passe"])){
+                    return $account["roleData"][0]["code"];
                 }
             }
         }
