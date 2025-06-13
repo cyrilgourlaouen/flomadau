9bd58e4e7de45f530a0d6bf4a238cd7a111bf2af
djulou <dorian.julou@gmail.com>
2025-06-13 14:01:45 +0200

    Fix update bug

--- html/Floma/templates/front/consultation/consultation.php
+++ html/Floma/templates/front/consultation/consultation.php
@@ -49,34 +49,37 @@
         </div>
     </form>
     <div class="personal-data">
-        <div class="title">
-            <h2>Changer de mot de passe</h2>
-        </div>
-        <form method="POST" class="lines-container">
-            <div class="lines">
-                <h3>Mot de passe actuel*</h3>
-                <div class="password">
-                    <input class="inputPassword" id="inputPassword" type="password" name="mdp" required>
-                    <button id="CheckPassword" type="submit">
-                        <p>Valider</p>
-                    </button>
-                </div>
-            </div>
-        </form>
-        <form method="POST" class="lines-container">
-            <div class="lines">
-                <h3>Nouveau mot de passe</h3>
-                <input id="newPassword" type="password" name="mdp_confirm" required disabled>
-            </div>
-            <div class="lines">
-                <h3>Confirmer le nouveau mot de passe</h3>
-                <input id="confirmPassword" type="password" name="mdp_confirm" required disabled>
-            </div>
-            <p id="message"></p>
-            <button id="submitPasswordBtn" type="submit">
-                <p>Changement de mot de passe</p>
+    <div class="title">
+        <h2>Changer de mot de passe</h2>
+    </div>
+
+    <form method="POST" class="lines-container" id="changePasswordForm">
+        <div class="lines">
+        <h3>Mot de passe actuel*</h3>
+        <div class="password">
+            <input class="inputPassword" id="inputPassword" type="password" name="current_password" required>
+            <button id="checkPasswordBtn" type="button">
+            <p>Valider</p>
             </button>
-        </form>
+        </div>
+        </div>
+
+        <div class="lines">
+        <h3>Nouveau mot de passe</h3>
+        <input id="newPassword" type="password" name="new_password" required disabled>
+        </div>
+
+        <div class="lines">
+        <h3>Confirmer le nouveau mot de passe</h3>
+        <input id="confirmPassword" type="password" name="confirm_password" required disabled>
+        </div>
+
+        <p id="message"></p>
+
+        <button id="submitPasswordBtn" type="submit" disabled>
+        <p>Changer de mot de passe</p>
+        </button>
+    </form>
     </div>
 </div>
 <script src="./js/_js-ConsultationMembre.js"></script>
