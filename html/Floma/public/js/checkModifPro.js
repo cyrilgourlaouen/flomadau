4c40717b8f52fee681f97eb53ee9b67661696751
lgf <jonas.brisson@etudiant.univ-rennes1.fr>
2025-06-16 18:17:03 +0200

    aa

--- html/Floma/public/js/checkModifPro.js
+++ html/Floma/public/js/checkModifPro.js
@@ -4,12 +4,15 @@ btnModifier.addEventListener('click', function() {
   //on affiche les elements cachés
   hide = document.getElementsByClassName('cache');
   for(let i=0; i<hide.length; i++){
-    hide[i].classlist.remove('hide');
+    hide[i].classList.remove('cache');
+    console.log(hide[i]);
   }
 
   //on active les inputs
   inputs = document.getElementsByClassName('not-active');
   for(let j=0; j<inputs.length; j++){
-    
+    inputs[j].disabled = false;
   }
+
+  btnModifier.classList.add('cache');
 });
